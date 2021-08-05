7759497354ad3242911f2d244a09427d77042688
anton.ayupov <anton.ayupov@gmail.com>
2021-08-05 18:50:16 +0300

    404-страница

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,7 +1,17 @@
+import styles from './styles.module.css';
+import {Button} from '@ya.praktikum/react-developer-burger-ui-components';
+
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <div className="pt-30">
+      <div className={styles.content}>
+        <h1 className={`${styles.title} text text_type_main-large mb-8`}>Такой страницы не&nbsp;найдено</h1>
+        <Button type="primary">
+          Перейти на главную
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default NotFoundPage;
