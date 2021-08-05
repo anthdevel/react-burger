ee51a6bf9eafd3aa3b31fe5083e0b63c08f6435f
anton.ayupov <anton.ayupov@gmail.com>
2021-08-05 19:24:18 +0300

    Страница входа

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,52 @@
+import styles from './styles.module.css';
+import {Button, Input} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Link} from 'react-router-dom';
+
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className={styles.root}>
+      <div className={styles.content}>
+        <form className="mb-20">
+          <h3 className="text text_type_main-medium mb-6">Вход</h3>
+          <div className={`${styles.inputWrapper} mb-6`}>
+            <Input
+              type="email"
+              placeholder="E-mail"
+              size="default"
+              value=""
+              onChange={() => {
+              }}
+            />
+          </div>
+          <div className={`${styles.inputWrapper} mb-6`}>
+            <Input
+              type="password"
+              placeholder="Пароль"
+              size="default"
+              value=""
+              icon="ShowIcon"
+              onChange={() => {
+              }}
+            />
+          </div>
+          <Button type="primary" onClick={() => {
+          }}>
+            Войти
+          </Button>
+        </form>
+        <p className="text text_type_main-default text_color_inactive mb-4">
+          Вы&nbsp;&mdash; новый пользователь?
+          {' '}
+          <Link to="/register" className={styles.link}>Зарегистрироваться</Link>
+        </p>
+        <p className="text text_type_main-default text_color_inactive">
+          Забыли пароль?
+          {' '}
+          <Link to="/forgot-password" className={styles.link}>Восстановить пароль</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
