1fd0e2a9b8391864c408bf162f670c87877c2b69
anton.ayupov <anton.ayupov@gmail.com>
2021-08-05 19:37:54 +0300

    Страница регистрации

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,57 @@
+import styles from './styles.module.css';
+import {Button, Input} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Link} from 'react-router-dom';
+
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div className={styles.root}>
+      <div className={styles.content}>
+        <form className="mb-20">
+          <h3 className="text text_type_main-medium mb-6">Регистрация</h3>
+          <div className={`${styles.inputWrapper} mb-6`}>
+            <Input
+              type="text"
+              placeholder="Имя"
+              size="default"
+              value=""
+              onChange={() => {
+              }}
+            />
+          </div>
+          <div className={`${styles.inputWrapper} mb-6`}>
+            <Input
+              type="email"
+              placeholder="E-mail"
+              size="default"
+              value=""
+              onChange={() => {
+              }}
+            />
+          </div>
+          <div className={`${styles.inputWrapper} mb-6`}>
+            <Input
+              type="password"
+              placeholder="Пароль"
+              size="default"
+              value=""
+              icon="ShowIcon"
+              onChange={() => {
+              }}
+            />
+          </div>
+          <Button type="primary" onClick={() => {
+          }}>
+            Зарегистрироваться
+          </Button>
+        </form>
+        <p className="text text_type_main-default text_color_inactive">
+          Уже зарегистрированы?
+          {' '}
+          <Link to="/login" className={styles.link}>Войти</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default RegisterPage;
